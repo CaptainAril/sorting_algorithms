4db60bb3aa1340053ea8e6f866326526efcf974a
CaptainAril <obeemmanuelifeoluwa@gmail.com>
2022-08-16 17:47:37 +0000

    Insertion sort

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,52 +1,42 @@
 #include "sort.h"
 
-
+/**
+ * insertion_sort_list - sorts a doubly linked list of integers
+ * in ascending order using the Insertion sort algorithm.
+ * @list: linked list to be sorted.
+ *
+ * Description: Prints the list after each time two elements are swapped.
+ */
 void insertion_sort_list(listint_t **list)
 {
-	listint_t *tmp, *grt, *current;
+	listint_t *current, *tmp, *pos;
 
-	current = NULL;
-	grt = NULL;
-	if (!list)
+	if (!list || !(*list) || !(*list)->next)
 		return;
 	current = (*list)->next;
 
-	tmp = malloc(sizeof(listint_t));
-	if (!tmp)
-		return;
-	printf("tmp created\n");
-
 	while (current)
 	{
-		printf("current: %d\n", current->n);
-		printf("current prev: %d\n", current->prev->n);
-		if (current->prev->n > current->n)
+		pos = current->prev;
+		tmp = current;
+		current = current->next;
+
+		while (pos && pos->n > tmp->n)
 		{
-			tmp = current;
-//			printf("tmp: %d\n", tmp->n);
-			grt = current->prev;
-//			printf("grt: %d\n", grt->n);
-			current = current->next;
-//			printf("new current: %d\n", current->n);
-			current->prev = grt;
-			grt->next = current;
-//			printf("list after pop: ");
-//			print_list(*list);
+			pos->next = tmp->next;
+			if (tmp->next)
+				tmp->next->prev = pos;
+			tmp->next = pos;
+			tmp->prev = pos->prev;
+			if (pos->prev)
+				pos->prev->next = tmp;
 
-			tmp->prev = grt->prev;
-			grt->prev = tmp;
-			grt->prev->next = tmp;
-			tmp->next = grt;
-			printf("tmpPrev->tmp->tmpNext = %d->%d->%d\n", tmp->prev->n, tmp->n, tmp->next->n);
-			printf("grtPrev->grt->grtNext = %d->%d->%d\n", grt->prev->n, grt->n, grt->next->n);
-			//	printf("tmp %d\n", tmp->prev-n);
-			printf("list after insert: ");
-			print_list(**list);
+			pos->prev = tmp;
+			pos = tmp->prev;
+			if (!pos)
+				*list = tmp;
+
+			print_list(*list);
 		}
-		else
-			current = current->next;
-		print_list(*list);
 	}
-
-/*	free(tmp); */
 }
